7b5e1b7c21f73ef36f45abd5a94014a3d27ebf2a
MK <komaei@live.com>
2018-03-07 13:33:46 +0330

    Registration Customer

--- Server/Sanaap.Api/Controllers/CustomersController.cs
+++ Server/Sanaap.Api/Controllers/CustomersController.cs
@@ -3,7 +3,6 @@ using Bit.Owin.Exceptions;
 using Sanaap.Dto;
 using Sanaap.Model;
 using System;
-using System.Data.Entity;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,58 +12,49 @@ namespace Sanaap.Api.Controllers
 {
     public class CustomersController : SanaapDtoSetController<CustomerDto, Customer>
     {
-        //public virtual ICustomersRepository CustomersRepository { get; set; }
-
         [Action]
-        //public virtual async Task<Customer> AddCustomer(Customer customer, CancellationToken cancellation)
-        //{
-        //    Customer findedCustomer = (await Repository.GetAllAsync(cancellation)).Where(cu => cu.NationalCode == customer.NationalCode).FirstOrDefault();
-        //    if (findedCustomer != null)
-        //    {
-        //        throw new Exception("قبلا ثبت نام کرده اید. لطفا وارد شوید.");
-        //    }
-
-        //    customer.OTP = GenerateRandomNo();
-        //    customer.IsActive = false;
-        //    SendOtpSms("0" + customer.Mobile, "کد ورودی شما در سناپ : " + findedCustomer.OTP);
-        //    return await Repository.AddAsync(customer, cancellation);
-        //}
-
-        //[Function]
-        //public virtual async Task<bool> ConfirmOTP(Customer customer, int otp, CancellationToken cancellation)
-        //{
-        //    Customer findedCustomer = (await Repository.GetAllAsync(cancellation))
-        //        .Where(cu => cu.Id == customer.Id).FirstOrDefault();
-        //    if (findedCustomer.OTP == otp)
-        //    {
-        //        return true;
-        //    }
-
-        //    else return false;
-        //}
-
-        [Function]
-        public virtual async Task<Customer> LoginCustomer(long nationalCode, long mobile, CancellationToken cancellation)
+        public virtual async Task<Customer> AddCustomer(Customer customer, CancellationToken cancellation)
         {
-            Customer findedCustomer = await (await Repository.GetAllAsync(cancellation))
-                .Where(cu => cu.NationalCode == nationalCode && cu.Mobile == mobile && cu.IsActive == true).FirstOrDefaultAsync(cancellation);
-            if (findedCustomer == null)
-                throw new ResourceNotFoundException("کاربری با این مشخصات یافت نشد. لطفا با دقت اطلاعات را وارد نمائید.");
-
-            return findedCustomer;
+            Customer existingCustomer = (await Repository.GetAllAsync(cancellation)).Where(cu => cu.NationalCode == customer.NationalCode).FirstOrDefault();
+            if (existingCustomer != null)
+                throw new Exception("قبلا با این کد ملی ثبت نام کرده اید. لطفا وارد شوید.");
+            customer.OTP = GenerateRandomNo();
+            customer.IsActive = false;
+            SendOtpSms("0" + customer.Mobile, "کد ورودی شما در سناپ : " + customer.OTP);
+            return await Repository.AddAsync(customer, cancellation);
         }
 
-        //private void SendOtpSms(string mobile, string content)
-        //{
+        [Function]
+        public virtual async Task<bool> ConfirmOTP(Guid customerId, int otp, CancellationToken cancellation)
+        {
+            Customer existingCustomer = (await Repository.GetAllAsync(cancellation)).Where(cu => cu.Id == customerId).FirstOrDefault();
+            if (existingCustomer.OTP == otp)
+            {
+                existingCustomer.IsActive = true;
+                await Repository.UpdateAsync(existingCustomer, cancellation);
+                return true;
+            }
+            else return false;
+        }
 
-        //}
+        [Function]
+        public virtual async Task<SingleResult<CustomerDto>> LoginCustomer(long nationalCode, long mobile, CancellationToken cancellationToken)
+        {
+            SingleResult<CustomerDto> customerDto = SingleResult.Create(DtoEntityMapper.FromEntityQueryToDtoQuery((await Repository.GetAllAsync(cancellationToken)))
+            .Where(cu => cu.NationalCode == nationalCode && cu.Mobile == mobile && cu.IsActive == true));
+            if (customerDto == null)
+                throw new ResourceNotFoundException("کاربری با این مشخصات یافت نشد. لطفا با دقت اطلاعات را وارد نمائید.");
+            return customerDto;
+        }
 
-        //private int GenerateRandomNo()
-        //{
-        //    int min = 1000;
-        //    int max = 9999;
-        //    Random random = new Random();
-        //    return random.Next(min, max);
-        //}
+        private void SendOtpSms(string mobile, string content)
+        {
+
+        }
+
+        private int GenerateRandomNo()
+        {
+            int min = 1000, max = 9999; Random random = new Random(); return random.Next(min, max);
+        }
     }
 }
