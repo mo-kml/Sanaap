f0e1decc88f5a650a8f7f33b14aff29277fda00d
MK <komaei@live.com>
2018-05-27 12:48:44 +0430

    Menu/MySosRequests

--- Client/Sanaap.App/ViewModels/MainViewModel.cs
+++ Client/Sanaap.App/ViewModels/MainViewModel.cs
@@ -8,34 +8,15 @@ namespace Sanaap.App.ViewModels
 {
     public class MainViewModel : BitViewModelBase
     {
-        public BitDelegateCommand Logout { get; set; }
-
         public BitDelegateCommand SubmitSosRequest { get; set; }
 
         public BitDelegateCommand SubmitSosRequestByCall { get; set; }
 
-        public BitDelegateCommand GoToMySosRequests { get; set; }
-
         public bool IsBusy { get; set; } = false;
 
         public MainViewModel(INavigationService navigationService,
-            ISecurityService securityService,
-            IDeviceService deviceService)
+            ISecurityService securityService, IDeviceService deviceService)
         {
-            Logout = new BitDelegateCommand(async () =>
-            {
-                IsBusy = true;
-                try
-                {
-                    await securityService.Logout();
-                    await navigationService.NavigateAsync("/Login");
-                }
-                finally
-                {
-                    IsBusy = false;
-                }
-            });
-
             SubmitSosRequest = new BitDelegateCommand(async () =>
             {
                 await navigationService.NavigateAsync("SubmitSosRequest");
@@ -44,11 +25,6 @@ namespace Sanaap.App.ViewModels
             SubmitSosRequestByCall = new BitDelegateCommand(() =>
             {
                 deviceService.OpenUri(new Uri("tel://02141558"));
-            });
-
-            GoToMySosRequests = new BitDelegateCommand(async () =>
-            {
-                await navigationService.NavigateAsync("MySosRequests");
             });
         }
     }
--- Client/Sanaap.App/ViewModels/MenuViewModel.cs
+++ Client/Sanaap.App/ViewModels/MenuViewModel.cs
@@ -7,11 +7,27 @@ namespace Sanaap.App.ViewModels
     {
         public BitDelegateCommand GoToMySosRequests { get; set; }
 
+        public BitDelegateCommand Logout { get; set; }
+
         public MenuViewModel(INavigationService navigationService)
         {
             GoToMySosRequests = new BitDelegateCommand(async () =>
             {
-                await navigationService.NavigateAsync("MySosRequests");
+                await navigationService.NavigateAsync("Menu/MySosRequests");
+            });
+
+            Logout = new BitDelegateCommand(async () =>
+            {
+                IsBusy = true;
+                try
+                {
+                    await securityService.Logout();
+                    await navigationService.NavigateAsync("/Login");
+                }
+                finally
+                {
+                    IsBusy = false;
+                }
             });
         }
     }
