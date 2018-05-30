9ea4d30753d73a21743d159d94dcd8fd2c879a2d
MK <komaei@live.com>
2018-05-30 16:04:47 +0330

    colorPrimary & colorPrimaryDark

--- Client/Sanaap.App/ViewModels/MainViewModel.cs
+++ Client/Sanaap.App/ViewModels/MainViewModel.cs
@@ -8,6 +8,10 @@ namespace Sanaap.App.ViewModels
 {
     public class MainViewModel : BitViewModelBase
     {
+        public BitDelegateCommand GoToMySosRequests { get; set; }
+
+        public BitDelegateCommand Logout { get; set; }
+
         public BitDelegateCommand SubmitSosRequest { get; set; }
 
         public BitDelegateCommand SubmitSosRequestByCall { get; set; }
@@ -17,6 +21,17 @@ namespace Sanaap.App.ViewModels
         public MainViewModel(INavigationService navigationService,
             ISecurityService securityService, IDeviceService deviceService)
         {
+            GoToMySosRequests = new BitDelegateCommand(async () =>
+            {
+                await navigationService.NavigateAsync("MySosRequests");
+            });
+
+            Logout = new BitDelegateCommand(async () =>
+            {
+                await securityService.Logout();
+                await navigationService.NavigateAsync("Main/Login");
+            });
+
             SubmitSosRequest = new BitDelegateCommand(async () =>
             {
                 await navigationService.NavigateAsync("SubmitSosRequest");
