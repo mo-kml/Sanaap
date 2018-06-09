c05a7885e7c8f477618e09cbcf41391b5c81636a
MK <komaei@live.com>
2018-06-09 11:58:58 +0330

    icon droid

--- SanaapOperatorApp/SanaapOperatorApp/App.xaml.cs
+++ SanaapOperatorApp/SanaapOperatorApp/App.xaml.cs
@@ -70,8 +70,8 @@ namespace SanaapOperatorApp
 
             containerRegistry.GetBuilder().Register<IClientAppProfile>(c => new DefaultClientAppProfile
             {
-                HostUri = new Uri("http://192.168.1.207:53148/"),       // Local
-                //HostUri = new Uri("http://84.241.25.3:8220/"),   // Server
+                //HostUri = new Uri("http://192.168.1.207:53148/"),       // Local
+                HostUri = new Uri("http://84.241.25.3:8220/"),            // Server
                 // OAuthRedirectUri = new Uri("Test://oauth2redirect"),
                 AppName = "Sanaap",
                 ODataRoute = "odata/Sanaap/"
