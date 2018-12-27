69cf0f0ad6b46c3bde3111ec4524fb8edc3bac4b
mohammad kamali <moka122014@gmail.com>
2018-12-27 18:18:57 +0330

    add insurance policy list view

---
+++ SanaapApp/Sanaap.App/ViewModels/Insurance/InsurancePolicyListViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Bit.ViewModel;
+using Prism.Navigation;
+using PropertyChanged;
+using Sanaap.Dto;
+using Sanaap.Enums;
+using System;
+using System.Collections.ObjectModel;
+
+namespace Sanaap.App.ViewModels.Insurance
+{
+    public class InsurancePolicyListViewModel : BitViewModelBase
+    {
+        public InsurancePolicyListViewModel(INavigationService navigationService)
+        {
+            CreatePolicy = new BitDelegateCommand(async () =>
+              {
+                  await navigationService.NavigateAsync("CreatePolicy");
+              });
+
+            ShowPolicy = new BitDelegateCommand<PolicyItemSource>(async (policy) =>
+              {
+                  NavigationParameters parameters = new NavigationParameters();
+                  parameters.Add("Policy", policy);
+
+                  await navigationService.NavigateAsync("CreatePolicy", parameters);
+              });
+        }
+        public ObservableCollection<PolicyItemSource> Policies { get; set; }
+
+        public BitDelegateCommand CreatePolicy { get; set; }
+
+        public BitDelegateCommand<PolicyItemSource> ShowPolicy { get; set; }
+    }
+
+    [AddINotifyPropertyChangedInterface]
+    public class PolicyItemSource : InsurancePolicyDto
+    {
+        public string ColorName { get; set; }
+
+        public string CarName { get; set; }
+
+        public string InsuranceTypeName => EnumHelper<InsuranceType>.GetDisplayValue(InsuranceType);
+    }
+}
---
+++ Server/Sanaap.Model/InsurancePolicy.cs
@@ -0,0 +1,21 @@
+﻿using Sanaap.Enums;
+
+namespace Sanaap.Model
+{
+    public class InsurancePolicy : BaseEntity
+    {
+        public int CarId { get; set; }
+
+        public int ColorId { get; set; }
+
+        public string ChasisNo { get; set; }
+
+        public string VIN { get; set; }
+
+        public string InsurerNo { get; set; }
+
+        public string PlateNumber { get; set; }
+
+        public InsuranceType InsuranceType { get; set; }
+    }
+}
