c2d0557d2199fc644723bdba06a42dce999cf755
Emad_AT <emadataolahi@yahoo.com>
2019-01-16 13:24:54 +0330

    All Entrys Have Been Updated

--- SanaapApp/Sanaap.App/App.xaml.cs
+++ SanaapApp/Sanaap.App/App.xaml.cs
@@ -125,7 +125,7 @@ namespace Sanaap.App
             containerRegistry.GetBuilder().Register<IClientAppProfile>(c => new DefaultClientAppProfile
             {
                 //HostUri = new Uri("http://84.241.25.3:8220/"),         // Server
-                HostUri = new Uri("http://cbba180d.ngrok.io"),
+                HostUri = new Uri("http://74644994.ngrok.io"),
                 AppName = "Sanaap",
                 ODataRoute = "odata/Sanaap/"
             }).SingleInstance();
