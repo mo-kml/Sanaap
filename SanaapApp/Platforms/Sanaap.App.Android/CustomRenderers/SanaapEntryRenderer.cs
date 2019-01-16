37a6179f82da3f537675f5d91abdb7d60fd8da6a
mohammad kamali <moka122014@gmail.com>
2019-01-16 11:07:35 +0330

    add sanaap entry custom view

---
+++ SanaapApp/Platforms/Sanaap.App.Android/CustomRenderers/SanaapEntryRenderer.cs
@@ -0,0 +1,29 @@
+﻿using Android.Content;
+using Sanaap.App.Droid.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(SanaapEntryRenderer))]
+namespace Sanaap.App.Droid.CustomRenderers
+{
+    public class SanaapEntryRenderer : EntryRenderer
+    {
+        public SanaapEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                Control.Background = null;
+
+                int padding = (Control.CompoundPaddingBottom + Control.CompoundPaddingEnd) / 2;
+
+                Control.SetPadding(10, padding, 20, padding);
+            }
+        }
+    }
+}
---
+++ SanaapApp/Sanaap.App/Controls/SanaapEntry.xaml.cs
@@ -0,0 +1,81 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sanaap.App.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SanaapEntry : ContentView
+    {
+        public SanaapEntry()
+        {
+            InitializeComponent();
+        }
+
+        public Color BorderColor
+        {
+            get => (Color)GetValue(BorderColorProperty);
+            set => SetValue(BorderColorProperty, value);
+        }
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public string FontFamily
+        {
+            get => (string)GetValue(FontFamilyProperty);
+            set => SetValue(FontFamilyProperty, value);
+        }
+
+        public string Placeholder
+        {
+            get => (string)GetValue(PlaceholderProperty);
+            set => SetValue(PlaceholderProperty, value);
+        }
+
+        public Color PlaceholderColor
+        {
+            get => (Color)GetValue(PlaceholderColorProperty);
+            set => SetValue(PlaceholderColorProperty, value);
+        }
+
+        public int MaxLength
+        {
+            get => (int)GetValue(MaxLengthProperty);
+            set => SetValue(MaxLengthProperty, value);
+        }
+
+        public Keyboard Keyboard
+        {
+            get => (Keyboard)GetValue(KeyboardProperty);
+            set => SetValue(KeyboardProperty, value);
+        }
+
+        public static readonly BindableProperty TextProperty =
+                BindableProperty.Create(nameof(Text), typeof(string), typeof(string), null);
+
+
+        public static readonly BindableProperty FontFamilyProperty =
+                BindableProperty.Create(nameof(FontFamily), typeof(string), typeof(string), null);
+
+        public static readonly BindableProperty PlaceholderProperty =
+                BindableProperty.Create(nameof(Placeholder), typeof(string), typeof(string), null);
+
+
+        public static readonly BindableProperty BorderColorProperty =
+                BindableProperty.Create(nameof(BorderColor), typeof(Color), typeof(Color), Color.FromHex("#e2e2e2"));
+
+        public static readonly BindableProperty PlaceholderColorProperty =
+                BindableProperty.Create(nameof(PlaceholderColor), typeof(Color), typeof(Color), Color.FromHex("#e2e2e2"));
+
+        public static readonly BindableProperty MaxLengthProperty =
+                BindableProperty.Create(nameof(MaxLength), typeof(int), typeof(int), int.MaxValue);
+
+        public static readonly BindableProperty KeyboardProperty =
+                BindableProperty.Create(nameof(Keyboard), typeof(Keyboard), typeof(Keyboard), Keyboard.Default);
+
+    }
+}
