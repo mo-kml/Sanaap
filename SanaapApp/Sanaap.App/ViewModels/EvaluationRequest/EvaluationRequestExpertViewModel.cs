e31dc2b8cc62d3fba20083d2382852f0c593a16d
mohammad kamali <moka122014@gmail.com>
2019-01-27 20:29:33 +0330

    evaluation request expert has been changed

--- SanaapApp/Sanaap.App/ViewModels/EvaluationRequest/EvaluationRequestExpertViewModel.cs
+++ SanaapApp/Sanaap.App/ViewModels/EvaluationRequest/EvaluationRequestExpertViewModel.cs
@@ -4,6 +4,9 @@ namespace Sanaap.App.ViewModels.EvaluationRequest
 {
     public class EvaluationRequestExpertViewModel : BitViewModelBase
     {
-
+        public EvaluationRequestExpertViewModel()
+        {
+            string[] a = typeof(MapViewModel).Assembly.GetManifestResourceNames();
+        }
     }
 }
