b3652b09e2376b93a25a9959ba7a745a951ea33d
mohammad kamali <moka122014@gmail.com>
2019-01-28 15:20:33 +0330

    register View has been completed

--- SanaapApp/Sanaap.App/App.xaml.cs
+++ SanaapApp/Sanaap.App/App.xaml.cs
@@ -73,7 +73,7 @@ namespace Sanaap.App
             //{
             //    await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(LoginView)}");
             //}
-            await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(LoginView)}");
+            await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(RegisterView)}");
 
 
             IEventAggregator eventAggregator = Container.Resolve<IEventAggregator>();
