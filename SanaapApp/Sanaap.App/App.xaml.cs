5e83c46d1d81a6f7cf3c98c3a49b51edc81e23cb
mohammad kamali <moka122014@gmail.com>
2019-02-02 14:50:48 +0330

    Insurance Policy changed

--- SanaapApp/Sanaap.App/App.xaml.cs
+++ SanaapApp/Sanaap.App/App.xaml.cs
@@ -73,7 +73,7 @@ namespace Sanaap.App
             //{
             //    await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(LoginView)}");
             //}
-            await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(ContactUsView)}");
+            await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(InsurancePolicyListView)}");
 
 
             IEventAggregator eventAggregator = Container.Resolve<IEventAggregator>();
