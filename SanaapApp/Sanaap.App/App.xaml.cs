32e0f71bbc1a36f76c9095912eebcd6e87ccb94b
emad ataolahi <emadataolahi@yahoo.com>
2019-02-25 18:09:36 +0330

    All Labels As Button Changed To Button

--- SanaapApp/Sanaap.App/App.xaml.cs
+++ SanaapApp/Sanaap.App/App.xaml.cs
@@ -72,16 +72,16 @@ namespace Sanaap.App
 
             bool isLoggedIn = await Container.Resolve<ISecurityService>().IsLoggedInAsync();
 
-            //if (isLoggedIn)
-            //{
-            //    await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(MainMenuView)}");
-            //}
-            //else
-            //{
-            //    await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(LoginView)}");
-            //}
+            if (isLoggedIn)
+            {
+                await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(MainMenuView)}");
+            }
+            else
+            {
+                await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(LoginView)}");
+            }
 
-            await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(EvaluationRequestExpertRankView)}");
+            //await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(EvaluationRequestFilesView)}");
 
 
             IEventAggregator eventAggregator = Container.Resolve<IEventAggregator>();
